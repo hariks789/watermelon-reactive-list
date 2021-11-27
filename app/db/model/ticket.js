4e288681b87f7f4375b5b0453e5fa869d8cea0ab
Hariks <hari.sunilkumar@mutualmobile.com>
2021-11-27 14:36:24 +0530

    Minor bug fix

--- app/db/model/ticket.js
+++ app/db/model/ticket.js
@@ -1,4 +1,15 @@
 import {Model} from '@nozbe/watermelondb';
+import {field} from '@nozbe/watermelondb/decorators';
+
 export default class Ticket extends Model {
   static table = 'tickets';
+
+  @field('name')
+  name;
+
+  @field('number')
+  number;
+
+  @field('remarks')
+  remarks;
 }
